6312463e00eed220c5ea3f6406f701498e7ebf98
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2019-08-20 23:36:07 +0200

    Print puzzle before solution

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,16 @@ use std::fs;
 
 fn main() {
     let board = Board::from_string(&fs::read_to_string("puzzle.txt").expect("Could not read puzzle.txt"));
+
+    println!("Puzzle to solve:");
+    board.cells.print();
+
     let solutions = board.solve();
 
     assert_eq!(solutions.len(), 1);
     assert_eq!(solutions.get(0).map(|it| it.is_solved()), Some(true));
 
+    println!("Solution:");
     for s in solutions {
         s.cells.print();
     }
